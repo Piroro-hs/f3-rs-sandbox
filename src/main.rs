15595b4d2addc567e8f3954da16e771cad932f27
Piroro-hs <Piroro-hs@users.noreply.github.com>
2020-10-21 13:20:18 +0900

    Blink and sleep mode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+use cortex_m::{asm, peripheral::syst::SystClkSource};
+use cortex_m_rt::{entry, exception};
 use panic_halt as _;
 use stm32f3xx_hal::{self as hal, pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
+    let mut dp_c = cortex_m::Peripherals::take().unwrap();
 
     let mut rcc = dp.RCC.constrain();
     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
 
     let mut led = gpiob.pb3.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
 
+    dp_c.SYST.set_clock_source(SystClkSource::External);
+    dp_c.SYST.set_reload(500_000);
+    dp_c.SYST.clear_current();
+    dp_c.SYST.enable_interrupt();
+    dp_c.SYST.enable_counter();
+
     loop {
         led.toggle().unwrap();
-        asm::delay(8_000_000);
+        asm::wfi();
     }
 }
+
+#[exception]
+fn SysTick() {}
